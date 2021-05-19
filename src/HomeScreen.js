25f52e2bc63f286df03cf87f7ccd42c65568a35f
thejasjanardhan <thejasj435@gmail.com>
2021-05-19 22:11:22 +0530

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Row from "./Row";
+import requests from "./requests";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      {/* Nav */}
+      <Nav />
+
+      {/* Banner */}
+      <Banner />
+
+      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOrwSAhsDtYdK3OEU0Es6FHmgOu8FUj9U",
+  authDomain: "netflix-build-aeef3.firebaseapp.com",
+  projectId: "netflix-build-aeef3",
+  storageBucket: "netflix-build-aeef3.appspot.com",
+  messagingSenderId: "223999917078",
+  appId: "1:223999917078:web:3f7d13b494f49f483fc937",
+};
